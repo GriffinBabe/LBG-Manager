df6c9ca1a0bd7f05af8949e411a54df65a127903
Stefano <stefanodonne@gmail.com>
2019-04-19 15:22:36 +0200

    UI

--- app/src/main/java/com/example/LBGManager/Channel.java
+++
@@ -1,9 +0,0 @@
-package com.example.LBGManager;
-
-public interface Channel {
-
-    /**
-     * This method is implemented and is called from LBG when the model changes.
-     */
-    void stateChanged();
-}
